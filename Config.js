6908ad72cb3c985dbcf6b1b93a1ab66d9d67dba7
queen-krish <165665509+queen-krish@users.noreply.github.com>
2024-06-09 11:23:55 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,18 +3,18 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "94787354843"],
+global.ownername = process.env.OWNER_NAME || "ᴀꜱᴀɴᴋᴀ ᴄʜᴀɴᴅɪᴍᴀ",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
 
-global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
+global.botname = process.env.BOTNAME || "★彡[Qᴜᴇᴇɴ-ᴋʀɪꜱʜ]彡★",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER || "Qᴜᴇᴇɴ-ᴋʀɪꜱʜ",
 global.packname = 'Sticker By'
-global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.author = process.env.AUTHOR || "ᴄʜᴀɴᴅɪᴍᴀ",
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
